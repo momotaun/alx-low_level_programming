8da21568e880ed4fff9abd70a5d67f783503967d
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-16 02:23:38 -0800

    C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program that prints a hardcoded extract with some special characters
+ * Return 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
