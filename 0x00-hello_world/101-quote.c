bf19f6d7d7cb94ad380000f7b93b7c22fa4d286c
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-16 02:38:33 -0800

    Fixed file for Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,15 @@
 /**
  * main - Program entrypoint
  *
- * Description: Program that prints a hardcoded extract with some special 
+ * Description: Program that prints a hardcoded extract with some special
  * characters
  * Return 1
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	char message[] = 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, message, 59);
-	return(1);
+	return (1);
 }
