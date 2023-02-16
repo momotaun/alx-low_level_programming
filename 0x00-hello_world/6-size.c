830aa828e65e2ddf79b7bf22fcd02fbe40d2941e
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-16 02:07:46 -0800

    C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *  main - Entry point
+ *  @argc: argument counter
+ *  @argv: argument vector
+ *
+ *  Description: Program that prints the size of various
+ *  types on the computer it is compiled and run on.
+ *  Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	if (strcmp(argv[0], "./size64"))
+		printf("Size of a long int: 4 byte(s)\n");
+	else
+		pritnf("Size of a long int: 8 byte(s)\n");
+	printf("Size of long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+
+	return (0);
+}
