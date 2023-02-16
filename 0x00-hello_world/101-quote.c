0d8ddc3581f2491e02485e501f7cbb2ff023c16b
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-16 02:29:53 -0800

    Fixing message for Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,15 @@
 /**
  * main - Program entrypoint
  *
- * Description: Program that prints a hardcoded extract with some special characters
+ * Description: Program that prints a hardcoded extract with some special 
+ * characters
  * Return 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf(
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		);
 
 	return(1);
 }
