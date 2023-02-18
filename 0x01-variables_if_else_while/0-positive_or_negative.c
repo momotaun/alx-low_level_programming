ea07afc05b96f40d13b763ca0cac16c5a9aa3912
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-18 00:08:20 -0800

    Added variable n and newline

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("is positive");
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("is negative");
+		printf("%d is negative\n", n);
 	else
 		printf("is zero");
 	return (0);
