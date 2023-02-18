8e0899c40c0e9f8f21732f28fedb598292570765
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-17 23:58:03 -0800

    Fixed syntax error of semicolon

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 	if (n > 0)
 		printf("is positive");
 	else if (n < 0)
-		printf("is negative")
+		printf("is negative");
 	else
-		printf("is zero")
+		printf("is zero");
 	return (0);
 }
