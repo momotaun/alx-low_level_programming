b075710bc68a8973a1389d8de9c53f2c2a5c12bb
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-18 00:31:15 -0800

    Added modulus in condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("The last digit of %d is ", n);
-	if (n > 5)
+	if (n % 10 > 5)
 		printf("and is greater than 5");
-	else if (n == 0)
+	else if (n % 10 == 0)
 		printf("and is 0");
 	else
 		printf("and is less than 6 and not 0");
