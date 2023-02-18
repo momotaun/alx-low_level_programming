650c0ec92aa6ed207e87e821700282778b0f959e
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-18 00:25:51 -0800

    Added conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,6 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	printf("The last digit of %d is ", n);
+	if (n > 5)
+		printf("and is greater than 5");
+	else if (n == 0)
+		printf("and is 0");
+	else
+		printf("and is less than 6 and not 0");
 	return (0);
 }
