808a90244397b2b62b10395dc282232f99d505c2
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 08:15:23 -0800

    Added new variable

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to display combibation of three numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+	int l = 49
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			while (k < 58)
+			{
+				while (l < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					putchar(44);
+					l++;
+				}
+				k++;
+				l = k + 1;
+			}
+			j++;
+			k = j + 1;
+		}
+		i++;
+		j = i + 1;
+		k = j + 1;
+	}
+	putchar(10);
+	return (0);
+}
