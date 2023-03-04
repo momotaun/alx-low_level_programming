dae6ee65623e120d2dc66eb4a5809a95458ccf75
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 04:27:48 -0800

    Program to print 0 to 9 separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to display 0 to 9 separated by comma
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i == 57)
+			putchar(44);
+		i++
+	}
+	return (0);
+}
