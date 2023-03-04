01ff6eaba1208c7042bf542f7980e57b1773541e
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 03:00:44 -0800

    Program to display lowercase alphabets excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to display lowercase aplhabets excluding e and q
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwqyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (i == 4 || i ==23)
+		{
+			continue;
+		}
+		putchar(alphabets[i]);
+	}
+	putchar(10);
+	return (0);
+}
