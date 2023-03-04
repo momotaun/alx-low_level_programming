0926e4a72a0697ae39d53c4e53ba135195086e41
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 08:51:36 -0800

    Added condition

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,6 +26,11 @@ int main(void)
 					putchar(32);
 					putchar(k);
 					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+					{
+						l++;
+						continue;
+					}
 					putchar(44);
 					putchar(32);
 					l++;
