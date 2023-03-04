def2dc8df9a78e98c4b1c8f1301127764158ca23
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 03:13:39 -0800

    Program to display numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to display 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers = 10;
+	int i = 0;
+
+	while (i < 10)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
