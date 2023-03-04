7ac948fa0959caf0572f87e5dcd0e68ffb34cc74
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 02:18:16 -0800

    Program to display alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program enty point
+ *
+ * Description: Program to display all alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (i < 52)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar(10);
+}
