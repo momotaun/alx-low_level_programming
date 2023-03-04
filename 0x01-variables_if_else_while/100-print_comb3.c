193e6cbe1056b9ed8fb535c556f5baf0f9391d9b
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 05:06:45 -0800

    Program to display different combinations of two digits without repeats

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to display combibation of two numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+	
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 8 && j != 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	return (0);
+}
