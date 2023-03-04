791c0400f179323d952b15e46f49d62ed822d320
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 08:00:55 -0800

    Added a fourth variable

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,8 +9,9 @@
 int main(void)
 {
 	int i = 48;
-	int j = 49;
-	int k = 50;
+	int j = 48;
+	int k = 48;
+	int l = 49;
 
 	while (i < 58)
 	{
@@ -18,17 +19,20 @@ int main(void)
 		{
 			while (k < 58)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i == 55 && j == 56 && k == 57)
+				while (l < 58)
 				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+					{
+						k++;
+						continue;
+					}
+					putchar(44);
+					putchar(32);
 					k++;
-					continue;
 				}
-				putchar(44);
-				putchar(32);
-				k++;
 			}
 			j++;
 			k = j + 1;
