e547a3b60fd679402bbead54049ca16824cd394e
momotaun <momotaun@student.wethinkcode.co.za>
2023-03-04 09:34:54 -0800

    Added readme

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entrypoint
+ *
+ * Description: Program to print _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
